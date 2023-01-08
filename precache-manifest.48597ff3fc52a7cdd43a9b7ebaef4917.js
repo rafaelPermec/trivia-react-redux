5004647f948b62f3ecf973e93b8563a56c14ed62
rafaelPermec <geekchef.dev@gmail.com>
2023-01-08 17:59:20 -0300

    Updates

---
+++ precache-manifest.48597ff3fc52a7cdd43a9b7ebaef4917.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdafad0f3c1aff464a073355d87a72f",
+    "url": "/trivia-react-redux/index.html"
+  },
+  {
+    "revision": "71b5fad9e0f69c474edd",
+    "url": "/trivia-react-redux/static/css/main.b9aadae2.chunk.css"
+  },
+  {
+    "revision": "2c3c4df772a9cb3e32c8",
+    "url": "/trivia-react-redux/static/js/2.33787e53.chunk.js"
+  },
+  {
+    "revision": "6b427be26341f18e5beeadca3932038e",
+    "url": "/trivia-react-redux/static/js/2.33787e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b5fad9e0f69c474edd",
+    "url": "/trivia-react-redux/static/js/main.6f7a5bb4.chunk.js"
+  },
+  {
+    "revision": "cab236066c8a027c2fd3",
+    "url": "/trivia-react-redux/static/js/runtime-main.b83dab60.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-react-redux/static/media/trivia.466d153e.png"
+  }
+]);
